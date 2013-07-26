c6385c85cbdaefba28cd70c27ec737c1a13ce68d
Aneesh Goel <aneesh.goel.rbtx@gmail.com>
2013-07-25 18:11:58 -0700

    hack hack hack

--- games/media/games/space_pirate/space_pirate.js
+++ games/media/games/space_pirate/space_pirate.js
@@ -55,6 +55,7 @@ undum.game.situations = {
                 system.setQuality('armor', 100);
                 system.write($("#board-station").html());
                 system.doLink('station-entrance');
+                system.setCharacterText("<p>Welcome to Artosis Station.</p>")
             }
         }
     ),
@@ -63,9 +64,95 @@ undum.game.situations = {
             if (from !== 'board-station') {
                 system.write($('#station-entrance-desc').html());
             }
-            system.write($('#station-entrance').html());
+            if (character.qualities.macguffin) {
+                system.write("<p><a href='escape'>There's no time to explore, get out of here!</a></p>");
+            } else {
+                system.write($('#station-entrance').html());
+            }
         }
-    })
+    }),
+    //TODO actually write this
+    'station-work': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-work').html());
+        },
+        act: function(character, system, action) {
+            if (action === 'spire') {
+                system.write("<p>The path to the spire is blocked by rubble.  You'll need to find another way.</p>");
+            } else if (action === 'command') {
+                if (character.sandbox.rec_to_command) {
+                    system.doLink('station-command');
+                } else {
+                    system.write("<p>Placeholder for problem blocking this path.</p>");
+                    if (character.qualities.verve >= 5) {
+                        system.write("<p>Placeholder for <a href='./solving'>solving</a> problem with verve.</p>");
+                    } else {
+                        system.write("<p>Placeholder for being unable to solve the problem.</p>");
+                    }
+                }
+            } else if (action === 'solving') {
+                system.write("<p>Placeholder for the actual solution.</p>");
+                character.sandbox.work_to_command = True;
+            } else if (action === 'security') {
+                system.write("<p>Placeholder for you can't go there yet.</p>");
+            }
+    }),
+    'station-medbay': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-medbay').html());
+        }
+    }),
+    'station-security': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-security').html());
+        }
+    }),
+    'station-offduty': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-offduty').html());
+        },
+        act: function(character, system, action) {
+            if (action === 'spire') {
+                system.write("<p>The path to the spire is blocked by rubble.  You'll need to find another way.</p>");
+            } else if (action === 'command') {
+                if (character.sandbox.rec_to_command) {
+                    system.doLink('station-command');
+                } else {
+                    system.write("<p>The bulkhead to the command deck is partially closed,\
+                        but a repair drone seems to be jammed in the door, preventing it\
+                        from shutting all the way.  The drone has been smashed beyond\
+                        your ability to fix by the closing door.</p>"
+                    );
+                    if (character.qualities.vim >= 5) {
+                        system.write("<p>You think you can <a href='./blast' class='once'>blast</a>\
+                            the broken drone out of the way and open a path through.</p>"
+                        );
+                    } else {
+                        system.write("<p>You don't see any way through the gap.</p>");
+                    }
+                }
+            } else if (action === 'blast') {
+                system.write("<p>You blow away enough pieces of the drone that you think you can\
+                        <a href='station-command'>fit through the gap.</p>");
+                character.sandbox.rec_to_command = true;
+            }
+        }
+    }),
+    'station-rec': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-rec').html());
+        }
+    }),
+    'station-command': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-command').html());
+        }
+    }),
+    'station-spire': new undum.Situation({
+        enter: function(character, system, from) {
+            system.write($('#station-command').html());
+        }
+    }),
 };
 
 // ---------------------------------------------------------------------------
@@ -89,6 +176,9 @@ undum.game.qualities = {
     armor: new undum.NonZeroIntegerQuality(
         "Suit Condition", {priority:"0004", group:'stats'}
     ),
+    macguffin: new undum.OnOffQuality(
+        "MacGuffin", {priority:"0001", group:'inventory'}
+    ),
 };
 
 // ---------------------------------------------------------------------------
@@ -110,5 +200,12 @@ undum.game.init = function(character, system) {
     character.qualities.verve = 1;
     character.qualities.vigor = 1;
     character.qualities.armor = 0;
+    character.qualities.macguffin = 0;
+
+    character.sandbox.rec_to_command = false;
+    character.sandbox.work_to_command = false;
+    character.sandbox.security_disabled = false;
+    character.sandbox.has_security_badge = false;
+
     system.setCharacterText("<p>???</p>");
 };
